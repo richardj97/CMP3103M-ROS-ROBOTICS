b8be989b27893687bf36a12ce0821ea23d70d18d
SimonWest71 <35995280+SimonWest71@users.noreply.github.com>
2019-02-19 19:23:03 +0000

    Add files via upload

--- tob_kinematics.py
+++ tob_kinematics.py
@@ -3,34 +3,11 @@ from std_msgs.msg import Float32
 from geometry_msgs.msg import Twist
 
 wheel_radius = .1
-robot_radius = 1
+robot_radius = .1
     
-class left_wheel():
-
-    def __init__(self):
-        print('Starting...')
-        self.pub = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size=10)
-        self.float32_sub = rospy.Subscriber("/wheel_vel_left",
-                                              Float32, self.callback)
-        self.pete = rospy.Publisher("/wheel_vel_left" ,
-                                   Float32, queue_size=10)
-                                   
-        
-        
-    def callback(self, data):
-        print(data.data)
-        
-        
-        
-        #t = Twist()
-        #t.linear.x = 0.3
-        #t.angular.z = 0.8
-        #self.pub.publish(t)
-        
-        
-                                        
-    # computing the forward kinematics for a differential drive
-    def forward_kinematics(w_l, w_r):
+    
+        # computing the forward kinematics for a differential drive
+def forward_kinematics(w_l, w_r):
         c_l = wheel_radius * w_l
         c_r = wheel_radius * w_r
         v = (c_l + c_r) / 2
@@ -39,7 +16,7 @@ class left_wheel():
     
     
     # computing the inverse kinematics for a differential drive
-    def inverse_kinematics(v, a):
+def inverse_kinematics(v, a):
         c_l = v - (robot_radius * a)
         c_r = v + (robot_radius * a)
         w_l = c_l / wheel_radius
@@ -48,8 +25,31 @@ class left_wheel():
     
     
     # inverse kinematics from a Twist message (This is what a ROS robot has to do)
-    def inverse_kinematics_from_twist(self, t):
+def inverse_kinematics_from_twist(self, t):
         return self.inverse_kinematics(t.linear.x, t.angular.z)
+        
+        
+class left_wheel():
+
+    def __init__(self):
+        print('Starting...')
+        self.pub = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size=10)
+        self.float32_sub = rospy.Subscriber("/wheel_vel_left",
+                                              Float32, self.callback)
+                                           
+        
+    def callback(self, data):
+        print(data.data)
+        (v,a) = forward_kinematics (data.data, 0)
+        
+        t = Twist()
+        t.linear.x = v
+        t.angular.z = a
+        self.pub.publish(t)
+        
+        
+                                        
+
 
 if __name__ == "__main__":
     
